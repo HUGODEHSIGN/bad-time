39dcea34cc544f76b049a5dcece4732eeccc82bc
HugoHsi <72082805+HugoHsi@users.noreply.github.com>
2023-12-22 17:50:57 -0500

    added project packages

---
+++ firebase-config.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF_4qmbnRnOouHIbfJSA5D5vTbqL5j2gw",
+  authDomain: "bad-time-4236d.firebaseapp.com",
+  projectId: "bad-time-4236d",
+  storageBucket: "bad-time-4236d.appspot.com",
+  messagingSenderId: "835968852655",
+  appId: "1:835968852655:web:f6264c408c77581b51ea5a",
+  measurementId: "G-2KE889SBHE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const analytics = getAnalytics(app);
